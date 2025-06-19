2ffc6c4b65c7bf9d90c66f076a03b3159cba9452
MNicer2004 <133644099+MNicer2004@users.noreply.github.com>
2025-06-19 19:37:36 +0800

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,73 @@
+
+# if else better way
+# age = 18
+# message = "Adult" if age >= 18 else "Minor" if age <= 17 else "Bata kapa" if age == 10 else "Error"
+# print(message)
+
+# if else normal
+# age = 21
+# if age == 21:
+#    print("You are 21 years old")
+# elif age < 21:
+#    print("You are less than 21 years old")
+# for x in range(5):
+#    print(x)
+
+'''
+fullName = "Mark Christian E. Nicer"
+age = 21
+print(f"Hello, my name is {fullName} and I am {age} years old.")
+print("Hello my name is " + fullName + " and I am " + str(age) + " years old.")
+print(age)
+print(fullName + " " + (str(age)))
+print(f"{fullName} is so handsome and he is {age} now.")
+
+# Input and Output
+name = input("What is your name? ")
+print(f"Hello, {name}!")
+
+age = input("How old are you?")
+print(f"You are {age} years old {name}!")
+
+qualified = False
+if qualified == True:
+    print("You are qualified")
+else:
+    print("You are not qualified")
+
+'''
+# converting text into int
+'''
+
+money = "1000"
+add = 2000
+text = "hi"
+
+print(text + " " + int(money + add))
+'''
+
+# basic calculator
+'''
+firstNumber = int(input("Enter your first number: "))
+secondNumber = int(input("Enter your second number: "))
+
+print("Do you want to add, subtract, multiply or divide? (a/s/m/d)")
+choice = input("Enter your choice: ")
+if choice == 'a':
+    result = firstNumber + secondNumber
+    print(
+        f"The result of adding {firstNumber} + {secondNumber} = {result}")
+elif choice == 's':
+    result = firstNumber - secondNumber
+    print(
+        f"The result of subtracting {firstNumber} - {secondNumber} = {result}")
+elif choice == 'm':
+    result = firstNumber * secondNumber
+    print(
+        f"The result of multiplying {firstNumber} * {secondNumber} = {result}")
+elif choice == 'd':
+    if secondNumber != 0:
+        result = firstNumber / secondNumber
+        print(
+            f"The result of dividing {firstNumber} / {secondNumber} = {result}")
+'''
---
+++ day2.py
@@ -0,0 +1,77 @@
+# List and Tuples
+
+'''
+# +Index       0       1      2
+# -Index      -3      -2     -1
+courses = ["BSIT", "BSCS", "BSBA"]
+print(courses[-3])
+'''
+
+# Reading List RANGE
+'''
+courses = ["BSIT", "BSCS", "BSBA", "BST", "BSHM"]
+print(courses[:3])
+#3: , 1:3
+'''
+
+# List LENGTH
+# len
+'''
+courses = ["BSIT", "BSCS", "BSBA", "BST", "BSHM"]
+print(len(courses))
+'''
+
+# List COUNT
+# variablename.count("What are u looking")
+'''
+name = ["Mark", "Mark", "Mork", "Mwark", "Mark"]
+print(name.count("Mark"))
+'''
+
+# List ADD items by APPEND()
+# syntax list.append(value) or ("string")
+'''
+catname = ["SUNNY", "SYOGI", "SIOPAO", "MEOWMEOW"]
+catname.append("soony")
+print(catname)
+'''
+# DELETE List Itemb by REMOVE()
+# syntax list.remove(value or "string")
+'''
+catname = ["SUNNY", "SYOGI", "SIOPAO", "MEOWMEOW"]
+catname.remove("SIOPAO")
+print(catname)
+'''
+# List DELETE ITEMS by DEL
+# syntax del list[index] or del list to delete all in the list
+'''
+catname = ["SUNNY", "SYOGI", "SIOPAO", "MEOWMEOW"]
+del catname[1]  # the syogi is deleted
+# del catname  # catname index is deleted
+print(catname)
+'''
+# Clearing a LIST
+# clear()
+# syntax list.clear()
+'''
+catname = ["SUNNY", "SYOGI", "SIOPAO", "MEOWMEOW"]
+catname.clear()  # just cleared the value in a list but still can access
+print(catname)
+'''
+# Combining a LIST by ADDING
+# use + operator to combine a lists
+'''
+catname = ["SUNNY", "SYOGI", "SIOPAO", "MEOWMEOW"]
+dogname = ["ARF", "DOGIE", "DOGS", "DAWG"]
+combinedlist = catname + dogname
+print(combinedlist) or print (catname + dogname)
+'''
+# SORT item LISTS
+# SORT list by alphabet or value
+# syntax list.sort() like a-z
+# syntax list.sort(reverse=True) like z-a
+'''
+names = ["Nyah", "Nicole", "Max", "Deku", "Luffy"]
+names.sort() #reverse=True for z-a
+print(names)
+'''
